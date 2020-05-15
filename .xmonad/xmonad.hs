02a8b4fafcb4efdef4f445c3660b7c65e8d8e3ba
Caio Rulli <caiorulli@gmail.com>
2020-05-15 00:55:46 -0300

    Xmobar config

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -11,26 +11,16 @@ myTerminal    = "st"
 myBorderWidth = 2
 myModMask     = mod4Mask
 
--- The default number of workspaces (virtual screens) and their names.
--- By default we use numeric strings, but any string may be used as a
--- workspace name. The number of workspaces is determined by the length
--- of this list.
---
--- A tagging example:
---
--- > workspaces = ["web", "irc", "code" ] ++ map show [4..9]
---
-myWorkspaces = ["1","2","3","4","5","6","7","8","music"]
+myWorkspaces = ["term","web","3","4","5","6","7","8","music"]
 
-myNormalBorderColor  = "#3B4252"
-myFocusedBorderColor = "#81A1C1"
+nord1 = "#3B4252"
+nord9 = "#81A1C1"
+
+myNormalBorderColor  = nord1
+myFocusedBorderColor = nord9
 
 myBar = "xmobar"
-  
--- Custom PP, configure it as you like. It determines what is being written to the bar.
-myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
-
--- Key binding to toggle the gap for the bar.
+myPP = xmobarPP { ppCurrent = xmobarColor nord9 "" . wrap "<" ">" }
 toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
 main = xmonad =<< statusBar myBar myPP toggleStrutsKey defaults
