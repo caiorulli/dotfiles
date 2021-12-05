2723f6a83d84510fc791b0d5d0641a04379c02ef
Caio Rulli Thomaz <caio@crthomaz.com.br>
2021-12-05 17:34:29 -0300

    Remove .lein

--- .lein/profiles.clj
+++
@@ -1,6 +0,0 @@
-{:user {:plugins [[lein-midje "3.2.2"]
-                  [lein-cljfmt "0.7.0"]
-                  [lein-ancient "0.6.15"]
-                  [lein-nvd "1.4.1"]]
-        :jvm-opts ["-Duser.language=en" "-Duser.country=US"]}
- :repl {:plugins [[cider/cider-nrepl "0.25.5"]]}}
