05decc681cdb99086618b96e3870d43f875e43c5
Caio Rulli Thomaz <caio@crthomaz.com.br>
2022-01-19 00:11:00 -0300

    Add spotify/smudge

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -85,3 +85,39 @@
                           (smtpmail-smtp-user     . "caio.rulli@quintoandar.com.br"))
                         t)))
 
+;; Spotify
+
+(map! :leader :prefix "t" :desc "Spotify" "s" #'global-smudge-remote-mode)
+
+(after! smudge
+  (setq smudge-oauth2-client-id "4d23f6619825421f85533eebe615fffd"
+        smudge-oauth2-client-secret (shell-command "pass spotify/smudge")
+        smudge-transport 'connect)
+
+  ;; A hydra for controlling spotify.
+  (defhydra hydra-spotify (:hint nil)
+    "
+^Search^                  ^Control^               ^Manage^
+^^^^^^^^-----------------------------------------------------------------
+_t_: Track               _SPC_: Play/Pause        _+_: Volume up
+_m_: My Playlists        _n_  : Next Track        _-_: Volume down
+_f_: Featured Playlists  _p_  : Previous Track    _x_: Mute
+_u_: User Playlists      _r_  : Repeat            _d_: Device
+^^                       _s_  : Shuffle           _q_: Quit
+"
+    ("t" smudge-track-search :exit t)
+    ("m" smudge-my-playlists :exit t)
+    ("f" smudge-featured-playlists :exit t)
+    ("u" smudge-user-playlists :exit t)
+    ("SPC" smudge-controller-toggle-play :exit nil)
+    ("n" smudge-controller-next-track :exit nil)
+    ("p" smudge-controller-previous-track :exit nil)
+    ("r" smudge-controller-toggle-repeat :exit nil)
+    ("s" smudge-controller-toggle-shuffle :exit nil)
+    ("+" smudge-controller-volume-up :exit nil)
+    ("-" smudge-controller-volume-down :exit nil)
+    ("x" smudge-controller-volume-mute-unmute :exit nil)
+    ("d" smudge-select-device :exit nil)
+    ("q" quit-window "quit" :color blue))
+
+  (map! :leader :prefix "o" :desc "Spotify" "s" #'hydra-spotify/body))
--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -3,3 +3,5 @@
 
 ;; General
 (package! paredit)
+
+(package! smudge)
