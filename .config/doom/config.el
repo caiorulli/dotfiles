21dab0ae184a0717421580e68a63137c76a2bb4c
Caio Rulli Thomaz <caio.rulli@quintoandar.com.br>
2022-01-26 10:43:02 -0300

    Add transparency only on linux
    (where I have xmonad available)

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -50,8 +50,10 @@
 
 ;; Transparency
 
-(set-frame-parameter (selected-frame) 'alpha '(90 . 90))
-(add-to-list 'default-frame-alist '(alpha . (90 . 90)))
+(when IS-LINUX
+  (progn
+    (set-frame-parameter (selected-frame) 'alpha '(90 . 90))
+    (add-to-list 'default-frame-alist '(alpha . (90 . 90)))))
 
 (defun transparency (value)
   "Sets the transparency of the frame window. 0=transparent/100=opaque"
