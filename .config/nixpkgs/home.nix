f40fa73f0c423545999c1dcc46b5fd921bafc08d
Caio Rulli Thomaz <caio@crthomaz.com.br>
2022-06-08 21:52:23 -0300

    move git and alacritty to home-manager

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -9,7 +9,6 @@
   home.packages = with pkgs; [
     direnv
     maim
-    bottom
     xh
     ripgrep
     fd
@@ -23,6 +22,68 @@
   programs = {
     exa.enable = true;
     bat.enable = true;
+    bottom.enable = true;
+
+    git = {
+      enable = true;
+      userName = "Caio Rulli Thomaz";
+      userEmail = "caio@crthomaz.com.br";
+      extraConfig = {
+        core.editor = "nvim";
+        pull.rebase = false;
+        init.defaultBranch = "deck";
+      };
+
+      difftastic.enable = true;
+    };
+
+    alacritty = {
+      enable = true;
+      settings = {
+        env.TERM = "xterm-256color";
+        window = {
+          opacity = 0.7;
+          padding = {
+            x = 5;
+            y = 5;
+          };
+        };
+        font = {
+          normal.family = "Fantasque Sans Mono";
+          size = 11.0;
+        };
+        colors = {
+          primary = {
+            background = "0x2E3440";
+            foreground = "0xD8DEE9";
+          };
+          cursor = {
+            text = "0x2E3440";
+            cursor = "0xD8DEE9";
+          };
+          normal = {
+            black = "0x3B4252";
+            red = "0xBF616A";
+            green = "0xA3BE8C";
+            yellow = "0xEBCB8B";
+            blue = "0x81A1C1";
+            magenta = "0xB48EAD";
+            cyan = "0x88C0D0";
+            white = "0xE5E9F0";
+          };
+          bright = {
+            black = "0x4C566A";
+            red = "0xBF616A";
+            green = "0xA3BE8C";
+            yellow = "0xEBCB8B";
+            blue = "0x81A1C1";
+            magenta = "0xB48EAD";
+            cyan = "0x8FBCBB";
+            white = "0xECEFF4";
+          };
+        };
+      };
+    };
   };
 
   services = {
