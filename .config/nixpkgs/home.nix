763b812c7ec7aabfcdca58c5994257a542739389
Caio Rulli Thomaz <caio@crthomaz.com.br>
2022-06-09 18:48:26 -0300

    move lots of applications to home-manager

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -1,27 +1,33 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
-  home.username = "caio";
-  home.homeDirectory = "/home/caio";
+  home = {
+    stateVersion = "22.05";
+    username = "caio";
+    homeDirectory = "/home/caio";
+    packages = with pkgs; [
+      # cli
+      direnv
+      maim
+      xh
+      ripgrep
+      fd
+      newsboat
+      gh
+      du-dust
+      neofetch
+      fzf
 
-  home.packages = with pkgs; [
-    # cli
-    direnv
-    maim
-    xh
-    ripgrep
-    fd
-    newsboat
-    gh
-    du-dust
-    neofetch
-    fzf
-
-    # applications
-    slack
-  ];
+      # applications
+      slack
+      discord
+      tdesktop
+      signal-desktop
+      spotify
+      thunderbird
+      brave
+    ];
+  };
 
   programs = {
     exa.enable = true;
@@ -162,14 +168,4 @@
       "x-scheme-handler/webcal" = "brave-browser.desktop";
     };
   };
-
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "22.05";
 }
