a9f7a9ccbf1d75e2e23fa2cfc12a39ac0c0e32a9
Caio Rulli Thomaz <caio@crthomaz.com.br>
2022-07-21 20:41:12 -0300

    ocsinventory-agent working!

--- pipovni.nix
+++ pipovni.nix
@@ -5,6 +5,33 @@
 { config, pkgs, ... }:
 
 let
+  ProcDaemon = pkgs.perlPackages.buildPerlPackage {
+    pname = "Proc-Daemon";
+    version = "0.23";
+    src = builtins.fetchurl {
+      url = "https://cpan.metacpan.org/authors/id/A/AK/AKREAL/Proc-Daemon-0.23.tar.gz";
+      sha256 = "0scv11s9847b761vvb9vyk61br9m11cfwz69mg5k3d28g5dvih1l";
+    };
+    propagatedBuildInputs = with pkgs.perlPackages; [
+      ProcProcessTable
+    ];
+  };
+  ProcPIDFile = pkgs.perlPackages.buildPerlPackage {
+    pname = "Proc-PID-File";
+    version = "1.29";
+    src = builtins.fetchurl {
+      url = "https://cpan.metacpan.org/authors/id/D/DM/DMITRI/Proc-PID-File-1.29.tar.gz";
+      sha256 = "196lbhszv4lri91d19jhb7a2yvq5n98jl35pd20ksb8qvx4xmkiv";
+    };
+  };
+  FileTemp = pkgs.perlPackages.buildPerlPackage {
+    pname = "Proc-Daemon";
+    version = "0.2311";
+    src = builtins.fetchurl {
+      url = "https://cpan.metacpan.org/authors/id/E/ET/ETHER/File-Temp-0.2311.tar.gz";
+      sha256 = "0jkgnfc4bzjr5iarw5mf4pwbsbglqzhrrnhi6gy85663yldxd412";
+    };
+  };
   Ocsinventory-Unix-Agent = pkgs.perlPackages.buildPerlPackage rec {
     pname = "Ocsinventory-Unix-Agent";
     version = "2.4.2";
@@ -13,7 +40,7 @@ let
       sha256 = "0ggywrqbm7xl5dgcl511q17ccjxrwdjrn01j9agism5aas65vnny";
     };
     propagatedBuildInputs = with pkgs.perlPackages; [
-      XMLSimple
+      XMLSimple IOSocketSSL CompressZlib NetSSLeay NetIP NetSNMP DigestMD5 LWP FileTemp ProcDaemon ProcPIDFile FileTemp LWPProtocolHttps
     ];
   };
   in
@@ -153,6 +180,13 @@ let
   programs = {
     zsh.enable = true;
     dconf.enable = true;
+  };
+
+  services.cron = {
+    enable = true;
+    systemCronJobs = [
+      "0 */1 * * *     root    ocsinventory-agent"
+    ];
   };
 
   virtualisation.docker.enable = true;
