771e95ed2fc27d2a2399630fcf320ffcda623d43
Caio Rulli Thomaz <caio@crthomaz.com.br>
2022-07-24 14:01:45 -0300

    made ocsinventory work! \o/

--- hosts/outerheaven.nix
+++ hosts/outerheaven.nix
@@ -9,7 +9,7 @@
     [ # Include the results of the hardware scan.
       /etc/nixos/hardware-configuration.nix
       <home-manager/nixos>
-      ../modules/window-management.nix
+      ../modules/window-management
       ../modules/bluetooth.nix
     ];
 
--- hosts/pipovni.nix
+++ hosts/pipovni.nix
@@ -32,6 +32,36 @@ let
       sha256 = "0jkgnfc4bzjr5iarw5mf4pwbsbglqzhrrnhi6gy85663yldxd412";
     };
   };
+  NetCUPS = pkgs.perlPackages.buildPerlPackage {
+    pname = "Net-CUPS";
+    version = "0.64";
+    src = builtins.fetchurl {
+      url = "https://cpan.metacpan.org/authors/id/N/NI/NINE/Net-CUPS-0.64.tar.gz";
+      sha256 = "11asx383ap24xpdp9yx2953x9fw05aqhwn9whi68ggv21zzpgg6p";
+    };
+  };
+  ParseEDID = pkgs.perlPackages.buildPerlPackage {
+    pname = "Parse-EDID";
+    version = "1.0.7";
+    src = builtins.fetchurl {
+      url = "https://cpan.metacpan.org/authors/id/G/GR/GROUSSE/Parse-EDID-1.0.7.tar.gz";
+      sha256 = "1wavcxfic3kr18cndqwwv7z5xw45z7hb19ad5cm8l69jb880zp0s";
+    };
+    propagatedBuildInputs = with pkgs.perlPackages; [
+      TestWarn
+    ];
+  };
+  NmapParser = pkgs.perlPackages.buildPerlPackage {
+    pname = "Nmap-Parser";
+    version = "1.37";
+    src = builtins.fetchurl {
+      url = "https://cpan.metacpan.org/authors/id/A/AP/APERSAUD/Nmap-Parser-1.37.tar.gz";
+      sha256 = "1p2fb7c4fimwb3wgvpvkvz9dpbb1yksilbmqx3ygfwf68698b6p3";
+    };
+    propagatedBuildInputs = with pkgs.perlPackages; [
+      XMLTwig
+    ];
+  };
   Ocsinventory-Unix-Agent = pkgs.perlPackages.buildPerlPackage rec {
     pname = "Ocsinventory-Unix-Agent";
     version = "2.4.2";
@@ -39,13 +69,25 @@ let
       url = "https://github.com/OCSInventory-NG/UnixAgent/releases/download/v2.4.2/${pname}-${version}.tar.gz";
       sha256 = "0ggywrqbm7xl5dgcl511q17ccjxrwdjrn01j9agism5aas65vnny";
     };
+    patches = [ ../patches/ocsinventory.patch ];
     propagatedBuildInputs = with pkgs.perlPackages; [
       XMLSimple IOSocketSSL CompressZlib NetSSLeay NetIP NetSNMP DigestMD5 LWP FileTemp ProcDaemon ProcPIDFile FileTemp LWPProtocolHttps
+      ParseEDID NetNetmask NetPing NmapParser
+      # NetCUPS
     ];
   };
 
   deps = with pkgs; [
+    # plugify
     Ocsinventory-Unix-Agent
+    nmap
+    dmidecode
+    read-edid
+    smartmontools
+    pciutils
+    nettools
+
+    # dev
     clojure
     clj-kondo
     babashka
@@ -69,7 +111,7 @@ let
     [ # Include the results of the hardware scan.
       /etc/nixos/hardware-configuration.nix
       <home-manager/nixos>
-      ../modules/window-management.nix
+      ../modules/window-management
       ../modules/bluetooth.nix
     ];
 
