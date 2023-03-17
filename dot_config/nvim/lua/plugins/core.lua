32da85173c0ad2429d9f396c715d9a8bcc774c6c
Caio Rulli Thomaz <caio@crthomaz.com.br>
2023-03-17 14:41:17 -0300

    add nvim-dap (no config yet)

--- dot_config/nvim/lua/plugins/core.lua
+++ dot_config/nvim/lua/plugins/core.lua
@@ -5,4 +5,41 @@ return {
       colorscheme = "tokyonight",
     },
   },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "help",
+        "html",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "vim",
+        "yaml",
+        "go",
+        "rust",
+        "clojure",
+        "haskell",
+        "java",
+      },
+    },
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",
+        "delve",
+      },
+    },
+  },
+
+  { "mfussenegger/nvim-dap" },
 }
