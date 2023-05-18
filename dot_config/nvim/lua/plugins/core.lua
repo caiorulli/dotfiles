b921bac0b06c32ad7862b0a2c18a9b10f0a3c4b4
Caio Rulli Thomaz <caio.rulli@uber.com>
2023-05-18 15:55:44 -0300

    Nvim configuration fixes

--- dot_config/nvim/lua/plugins/core.lua
+++ dot_config/nvim/lua/plugins/core.lua
@@ -1,45 +1,43 @@
 return {
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "tokyonight",
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "tokyonight",
+        },
     },
-  },
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "bash",
-        "help",
-        "html",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "vim",
-        "yaml",
-        "go",
-        "rust",
-        "clojure",
-        "haskell",
-        "java",
-      },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = {
+                "bash",
+                "html",
+                "json",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "query",
+                "regex",
+                "vim",
+                "yaml",
+                "go",
+                "rust",
+                "clojure",
+                "haskell",
+                "java",
+                "python",
+            },
+        },
     },
-  },
 
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "gopls",
-        "delve",
-      },
+    {
+        "williamboman/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "gopls",
+                "delve",
+            },
+        },
     },
-  },
-
-  { "mfussenegger/nvim-dap" },
 }
