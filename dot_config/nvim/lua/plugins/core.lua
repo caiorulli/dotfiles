57566c0c6b69d8e74729616aaa3ea9e41faba579
Caio Rulli Thomaz <caio.rulli@uber.com>
2023-08-31 19:42:48 -0300

    Neovim changes

--- dot_config/nvim/lua/plugins/core.lua
+++ dot_config/nvim/lua/plugins/core.lua
@@ -1,43 +1,63 @@
 -- Refer for examples here: https://www.lazyvim.org/configuration/examples
 
 return {
-    {
-        "LazyVim/LazyVim",
-        opts = {
-            colorscheme = "tokyonight",
-        },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
     },
+  },
 
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = {
-            ensure_installed = {
-                "go",
-                "java",
-                "python",
-                "haskell",
-                "rust",
-                "lua",
-                "bash",
-                "html",
-                "json",
-                "markdown",
-                "markdown_inline",
-                "query",
-                "regex",
-                "vim",
-                "yaml",
-                "nix",
-            },
-        },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "go",
+        "java",
+        "python",
+        "haskell",
+        "rust",
+        "lua",
+        "bash",
+        "html",
+        "json",
+        "markdown",
+        "markdown_inline",
+        "query",
+        "regex",
+        "vim",
+        "yaml",
+        "nix",
+      },
     },
+  },
 
-    {
-        "williamboman/mason.nvim",
-        opts = {
-            ensure_installed = {
-                "gopls",
-            },
-        },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",
+      },
     },
+  },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      {
+        "<leader>fu",
+        function()
+          require("telescope.builtin").lsp_references()
+        end,
+        desc = "Find usages",
+      },
+      {
+        "<leader>bi",
+        function()
+          require("telescope.builtin").buffers()
+        end,
+        desc = "Pick buffer",
+      },
+    },
+  },
 }
