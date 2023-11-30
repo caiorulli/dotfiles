f696b38ebf7dea17c809016ef838101173ba7dea
Caio Rulli Thomaz <caiorulli@gmail.com>
2023-11-30 00:42:16 -0300

    Ilion initial commit
    Other hosts are deprecated. Ilion is fully NixOS operated.

--- README.md
+++ README.md
@@ -9,5 +9,4 @@
 My dotfiles! An outrageous amount of time was wasted here. Feel free to browse
 it, and check out if there's anything useful for your own configuration.
 
-Managed by [chezmoi](https://www.chezmoi.io/). It's mostly plain dotfiles, and
-a Pacman package list for quick reinstall if needed.
+Here you will find my NixOS configuration.
---
+++ home.nix
@@ -0,0 +1,107 @@
+{ config, pkgs, ... }: 
+
+{
+  home = {
+    stateVersion = "23.05";
+    enableNixpkgsReleaseCheck = true;
+
+    packages = with pkgs; [
+      firefox
+      spotify
+      discord
+
+      alacritty
+      zellij
+      bat
+      bottom
+
+      wget
+      ripgrep
+      fd
+      du-dust
+      neofetch
+      cmatrix
+      unzip
+      tig
+      lazygit
+    ];
+
+    sessionPath = [ "${config.home.homeDirectory}/.local/bin" ];
+
+    sessionVariables = {
+      LESSHISTFILE = "-";
+      PROTON_LOG_DIR = "${config.xdg.dataHome}/proton_log";
+      PROTON_LOG = 1;
+      XMONAD_CONFIG_HOME = "${config.xdg.configHome}/xmonad";
+      XMONAD_DATA_HOME = "${config.xdg.dataHome}/xmonad";
+      XMONAD_CACHE_HOME = "${config.xdg.cacheHome}/xmonad";
+    };
+  
+  
+    file = {
+      # xmonad
+      "${config.xdg.configHome}/xmonad/xmonad.hs".source =
+        ./xmonad/xmonad.hs;
+      "${config.xdg.configHome}/xmobar/xmobarrc".source =
+        ./xmonad/xmobarrc;
+ 
+      # neovim
+      "${config.xdg.configHome}/nvim".source = ./nvim;
+
+      # zellij
+      "${config.xdg.configHome}/zellij".source = ./zellij;
+    };
+  };
+
+  
+  programs = {
+    zsh = {
+      enable = true;
+      enableAutosuggestions = true;
+      defaultKeymap = "emacs";
+
+      history = {
+        path = "${config.xdg.dataHome}/zsh/history";
+        save = 1000000000;
+        size = 1000000000;
+      };
+    };
+
+    git = {
+      enable = true;
+      userName = "Caio Rulli Thomaz";
+      userEmail = "caiorulli@gmail.com";
+
+      aliases = {
+        s = "status";
+        co = "checkout";
+      };
+
+      extraConfig = {
+        core.editor = "nvim";
+        pull.rebase = false;
+        init.defaultBranch = "deck";
+      };
+
+      difftastic.enable = true;
+    };
+
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+    };
+
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
+
+    starship.enable = true;
+    bat.enable = true;
+    bottom.enable = true;
+    fzf.enable = true;
+    jq.enable = true;
+  };
+}
